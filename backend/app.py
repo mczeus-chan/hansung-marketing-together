a901f3020988a9faac84f2c3a1250ac1e704feea
mczeus-chan <ckim2244@gmail.com>
2026-05-18 19:40:42 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from routes.shortform import shortform_bp
 from routes.cardnews import cardnews_bp
 import os
 
-app = Flask(__name__, static_folder="../frontend")
+app = Flask(__name__, static_folder="../frontend", static_url_path="")
 CORS(app)
 
 # API 라우트 등록
